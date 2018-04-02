2cace511eaca7cc22c1fa045e305eac0a96e3167
Pavel Ulasavets <pavel_ulasavets@epam.com>
2018-04-02 15:30:23 +0200

    Lab 2. Implement switching betwee drawing primitives

--- src/EditorView/EditorModel.java
+++ src/EditorView/EditorModel.java
@@ -8,6 +8,10 @@ import shared.collections.*;
 import shared.visitors.IVisitable;
 import shared.visitors.IVisitor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class EditorModel implements IVisitable {
 
     private VisitableArrayList<GeometricFigure2D> figures;
@@ -23,16 +27,22 @@ public class EditorModel implements IVisitable {
         this.figures.add(this.activeFigureFactory.createFigure(transaction));
     }
 
+    public void setActiveFigureFactory(GeometricFigureFactory factory) {
+        this.activeFigureFactory = factory;
+    }
+
+    public HashMap<String, GeometricFigureFactory> getFactories() {
+        HashMap<String, GeometricFigureFactory> factoriesMap = new HashMap<>();
+
+        factoriesMap.put("Circle", new CircleFactory());
+        factoriesMap.put("Line", new LineFactory());
+        factoriesMap.put("Rectangle", new RectangleFactory());
+
+        return factoriesMap;
+    }
+
     public void accept(IVisitor v) {
         this.figures.accept(v);
-    }
-
-    public void clear() {
-        this.figures.clear();
-    }
-
-    public void removeAllSelected() {
-        // TODO: to be implemented
     }
 
 }
--- src/EditorView/FiguresPalette/FiguresPaletteController.java
+++ src/EditorView/FiguresPalette/FiguresPaletteController.java
@@ -2,10 +2,72 @@ package EditorView.FiguresPalette;
 
 import EditorView.EditorModel;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import shared.factories.GeometricFigureFactory;
+
+import java.util.HashMap;
+
 public class FiguresPaletteController {
-    private EditorModel model;
+
+    private EditorModel model = null;
+
+    @FXML
+    private VBox paletteButtonsContainer = null;
+
+
+    // ----------- private methods
+
+    private Button createButton(String name, GeometricFigureFactory factory) {
+        EditorModel model = this.model;
+        Button paletteButton = new Button(name);
+
+        paletteButton.getStyleClass().add("palette-button");
+        paletteButton.setPrefWidth(150);
+
+        EventHandler<? super MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                model.setActiveFigureFactory(factory);
+            }
+        };
+        paletteButton.setOnMousePressed(eventHandler);
+
+        return paletteButton;
+    }
+
+    private void intializeButtons() {
+        if (this.model == null) {
+            return;
+        }
+
+        HashMap<String, GeometricFigureFactory> factories = this.model.getFactories();
+
+        for (String key: factories.keySet()) {
+            this.paletteButtonsContainer.getChildren().add(this.createButton(key, factories.get(key)));
+        }
+
+    }
+
+    // ----------- public methods ---------------------
+
+    @FXML
+    public void initialize() {
+        this.intializeButtons();
+    }
 
     public void setModel(EditorModel model) {
         this.model = model;
+        this.intializeButtons();
+    }
+
+
+    // ---------- event handlers --------------
+
+    public void onMousePressed(MouseEvent e) {
+        System.out.println("on button clicked.");
     }
 }
